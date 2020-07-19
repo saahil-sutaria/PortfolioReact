0ab2d520aee6b27e559739e4c55fd82c35437d12
Sahil Sutaria <saahil.sutaria@gmail.com>
2020-07-19 12:58:42 -0700

    added image

---
+++ src/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+
+const Side = () => {
+    return(
+
+        <p>working</p>
+    );
+}
+
+
+
+export default Side
