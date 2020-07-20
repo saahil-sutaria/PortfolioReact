3676ecfeab262684e351aa85c3ff6b03c7836ceb
Sahil Sutaria <saahil.sutaria@gmail.com>
2020-07-19 18:39:15 -0700

    done for the day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import Background from './MainBackGround.png';
-import React, {useState} from 'react';
-import logo from './logo.svg';
+
+import React, {} from 'react';
+import Confetti from 'react-confetti'
+import * as Scroll from 'react-scroll';
+import { Link } from 'react-scroll'
+import Content from './SideBar'
 import './App.css';
-import Side from './SideBar'
+import Typical from 'react-typical'
 
 
 function App() {
 
-  return (
-    <div className="App">
-        <div style={{
-            backgroundImage: `url("")`,
+    return (
+        <div>
+            <div className="fillscreen" id="">
+                <Confetti
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    numberOfPieces = {500}
+                    recycle = {false}
+                    friction={0.99}
+                />
 
-        }}>
+                <div className="frontpage text">
+                    <p id="color">Software Engineer</p>
+                    <Typical
+                        id ="color"
+                        steps={['Located in SF Bay Area 👾', 5000 ]}
+                        loop={1}
+                        wrapper="r"
+                    />
+
+
+                </div>
+                <div className="container">
+                    <div className="center" >
+                        <Link
+                            activeClass = "active"
+                            to ="#section2"
+
+                            smooth={true}
+
+                            duration={1500}>
+                            Move this page
+                        </Link>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className = "followup_div" id="#section2" >
+                <Content/>
+            </div>
 
         </div>
 
-      <Side></Side>
-    </div>
+
 
   );
 }
 
 export default App;
+
+
+
+/*
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+
+
+const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            height: 140,
+            width: 100,
+        },
+        control: {
+            padding: theme.spacing(2),
+        },
+    }));
+    const [spacing, setSpacing] = React.useState(2);
+    const classes = useStyles();
+    const handleChange = (event) => {
+        setSpacing(Number(event.target.value));
+    };
+
+
+<Grid container className={classes.root} spacing={2}>
+    <Grid item xs={12}>
+    <Grid container direction="column" justify="center" alignItems="center" justify="center" spacing={2}>
+    {[0, 1, 2].map((value) => (
+    <Grid key={value} item>
+        <Paper className={classes.paper} />
+    </Grid>
+))}
+</Grid>
+</Grid>
+
+</Grid>
+<Paper className={classes.paper} /> */
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
-const Side = () => {
+const Content = () => {
     return(
 
-        <p>working</p>
+        <p>worasdfasdfasdking</p>
     );
 }
 
 
 
-export default Side
+export default Content
